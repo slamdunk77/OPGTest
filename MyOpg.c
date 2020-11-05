78d4762eea3f594b180a8f3d57d3ce7f3c7df529
slamdunk77 <62142444+slamdunk77@users.noreply.github.com>
2020-11-06 03:20:47 +0800

    Update MyOpg.c

--- MyOpg.c
+++ MyOpg.c
@@ -47,7 +47,7 @@ int findint(char ch){
 		case '(':t=3;break;
 		case ')':t=4;break;
 		case '#':t=5;break;
-//		default:syn=-1;break;
+		default:t=-1;break;
 	}
 	return t;
 }
@@ -67,7 +67,7 @@ void analyse(){
 		a=findint(zh);
 		b=findint(ch);
 		if(a==-1||b==-1){
-//			printf("E\n");
+			printf("E\n");
 			return;
 		}
 		
@@ -113,10 +113,10 @@ void loadch(){
 	zhongjian[zi-1]='#';
 }
 
-void scaner(){
+void scaner(char ch1){
 	syn=1;
-	ch=zhongjian[p++];
-	switch(ch){
+//	ch=zhongjian[p++];
+	switch(ch1){
 		case '+':
 		case '*':
 		case '(':
@@ -128,19 +128,19 @@ void scaner(){
 	} 
 }
 
-void saomiao(){
-	p=0;
-	q=0;
-	do{
-		scaner();
-		if(syn==-1){
-			printf("E\n");
-			break; 
-		} 
-		//当读到末尾 # 时，使用syn标记，syn=0 
-	}while(syn!=0);
-	
-}
+//void saomiao(){
+//	p=0;
+//	q=0;
+//	do{
+//		scaner();
+//		if(syn==-1){
+//			printf("E\n");
+//			break; 
+//		} 
+//		//当读到末尾 # 时，使用syn标记，syn=0 
+//	}while(syn!=0);
+//	
+//}
 
 int main(int argc, char *argv[]){
 	//读入句子到prog，并以 # 结尾
@@ -148,7 +148,7 @@ int main(int argc, char *argv[]){
 	loadch();
 	
 	//查看输入的句子中的字符是否合法，并把去掉空格的句子存入zhongjian[] 
-	saomiao();
+//	saomiao();
 	
 	//若读入的句子合法且完整 
 	if(syn==0){
